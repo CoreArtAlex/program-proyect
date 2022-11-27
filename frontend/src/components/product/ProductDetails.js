d647d1ee44274297be0afa011dbc52aff1b5b08d
alejandroguerravillegas <coreartalex2@gmail.com>
2022-11-27 09:40:01 -0800

    Adding Shopping Cart

--- frontend/src/components/product/ProductDetails.js
+++ frontend/src/components/product/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { getProductDetails, clearErrors } from "../../actions/productActions";
 import { useDispatch, useSelector } from "react-redux";
 import { useAlert } from "react-alert";
@@ -6,9 +6,11 @@ import Loader from "../layout/Loader";
 import MetaData from "../layout/MetaData";
 import { Carousel } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { addItemToCart } from "../../actions/cartActions";
 
-const ProductDetails = () => {
+const ProductDetails = ({match}) => {
   console.log("ProductDetails");
+  const [quantity, setQuantity] = useState(1)
   const dispatch = useDispatch();
   const alert = useAlert();
   
@@ -29,6 +31,29 @@ const ProductDetails = () => {
       dispatch(clearErrors());
     }
   }, [dispatch, alert, error, params.id]);
+
+  const addToCart = () =>{
+    dispatch(addItemToCart(match.params.id, quantity));
+    alert.success('Item Added to Cart');
+  }
+
+  const increaseQty = () =>{
+    const count = document.querySelector('.count');
+
+    if (count.valueAsNumber >= product.stock) return;
+
+    const qty = count.valueAsNumber + 1;
+    setQuantity(qty)
+  }
+
+  const decreaseQty = () =>{
+    const count = document.querySelector('.count');
+
+    if (count.valueAsNumber <= 1) return;
+
+    const qty = count.valueAsNumber - 1;
+    setQuantity(qty)
+  }
 
   return (
     <Fragment>
@@ -67,21 +92,23 @@ const ProductDetails = () => {
               <hr />
               <p id="product_price">${product.price}</p>
               <div className="stockCounter d-inline">
-                <span className="btn btn-danger minus">-</span>
+                <span className="btn btn-danger minus" onClick={decreaseQty}>-</span>
 
                 <input
                   type="number"
                   className="form-control count d-inline"
+                  value={quantity}
                   readOnly
                 />
 
-                <span className="btn btn-primary plus">+</span>
+                <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
               </div>
               <button
                 type="button"
                 id="cart_btn"
                 className="btn btn-primary d-inline ml-4"
                 disabled={product.stock === 0}
+                onClick ={addToCart}
               >
                 Add to Cart
               </button>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,6 +13,7 @@ import {
   userReducer,
   forgotPasswordReducer,
 } from "./reducers/userReducers";
+import { cartReducer } from "./reducers/cartReducers";
 
 const reducer = combineReducers({
   products: productsReducer,
@@ -20,9 +21,15 @@ const reducer = combineReducers({
   auth: authReducer,
   user: userReducer,
   forgotPassword: forgotPasswordReducer,
+  cart: cartReducer
 });
 
-let initialState = {};
+let initialState = {
+  cart: {
+    cartItems: localStorage.getItem('cartItems') 
+        ? JSON.parse(localStorage.getItem('cartItems')) : []
+  }
+};
 
 const middlware = [thunk];
 
